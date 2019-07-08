fb84fc2488ccedd58a57951f081b354fa7d1c7fc
Artem Kravchenko <ps4ever1991@gmail.com>
2019-07-08 21:50:49 +0900

    test some things

---
+++ Assets/Scripts/SomeManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeManager : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Some Debug");
+    }
+}
